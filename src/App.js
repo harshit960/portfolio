836dae03043360d41b131861a97b8049d4bbbc7a
harshit960 <raj.harshit960@gmail.com>
2023-04-17 12:13:25 +0530

    page added

--- src/App.js
+++ src/App.js
@@ -59,7 +59,10 @@ function App() {
             </div>
             <div className='absolute bottom-24 px-6'>
               <div className='head'>
-                OPEN <br />TO <br />HIRE
+                VIDEO EDITING
+              </div>
+              <div className='head text-lg'>
+                WITH PREMIER PRO
               </div>
               <div className='smallhead bottom-10 '>
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.
@@ -77,17 +80,15 @@ function App() {
         </div>
       </div>
 
-
       <div className='snap-start snap-always'>
         <div className=' snap-always h-screen'>
-          <div className='relative h-screen page1 '>
+          <div className='relative h-screen page2 overflow-hidden '>
+
             <div className='absolute h-64 w-full top_shadow'>
 
             </div>
-            <div className='absolute top-0  w-full h-20 flex navbar	mix-blend-overlay'>
-              <div className=' text-5xl font-black logo px-6 py-4 '>
-                LOGO
-              </div>
+            <div className='absolute top-0  w-full h-20 flex 	mix-blend-overlay'>
+
               <div className='text-black right-0  py-5 px-6 absolute'>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-10 h-10">
                   <path fillRule="evenodd" d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z" clipRule="evenodd" />
@@ -100,12 +101,65 @@ function App() {
             </div>
             <div className='absolute bottom-24 px-6'>
               <div className='head'>
-                OPEN <br />TO <br />HIRE
+                WEB DEV 
+              </div>
+              <div className='head text-lg'>
+                WITH REACT.JS
               </div>
               <div className='smallhead bottom-10 '>
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.
               </div>
             </div>
+            <video autoPlay muted loop
+              className="absolute -z-10 w-auto 
+            min-w-full min-h-full max-w-none items-center justify-center opacity-80">
+              <source src=
+                "https://downloads.videezy.com/system/protected/files/000/017/978/ICON_VERSION1.mp4"
+                type="video/mp4" />
+            </video>
+
+          </div>
+        </div>
+      </div>
+
+
+      <div className='snap-start snap-always'>
+        <div className=' snap-always h-screen'>
+          <div className='relative h-screen page2 overflow-hidden '>
+
+            <div className='absolute h-64 w-full top_shadow'>
+
+            </div>
+            <div className='absolute top-0  w-full h-20 flex 	mix-blend-overlay'>
+
+              <div className='text-black right-0  py-5 px-6 absolute'>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-10 h-10">
+                  <path fillRule="evenodd" d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z" clipRule="evenodd" />
+                </svg>
+
+              </div>
+            </div>
+            <div className='absolute h-64 w-full bottom-0 bottom_shadow'>
+
+            </div>
+            <div className='absolute bottom-24 px-6'>
+              <div className='head'>
+                STATIC <br/> WEBSITE
+              </div>
+              <div className='head text-lg'>
+                WITH WORDPRESS
+              </div>
+              <div className='smallhead bottom-10 '>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.
+              </div>
+            </div>
+            <video autoPlay muted loop
+              className="absolute -z-10 w-auto 
+            min-w-full min-h-full max-w-none items-center justify-center opacity-80">
+              <source src=
+                "https://storagggeee.blob.core.windows.net/filess/wordpressbg.mp4"
+                type="video/mp4" />
+            </video>
 
           </div>
         </div>
@@ -125,3 +179,4 @@ function App() {
 }
 
 export default App;
+
